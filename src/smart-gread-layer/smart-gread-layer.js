766363bbfb90e14036ca85c85f7f0edd2feddfff
vovantais <vladimir0297@mail.ru>
2021-01-08 21:31:12 +0300

    init:add source-dashboard

--- src/smart-gread-layer/smart-gread-layer.js
+++
@@ -1,2 +0,0 @@
-import "./smart-gread-layer.scss";
-
--- test/sum.spec.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-var */
-import { sumMy } from "../src/js/common";
-
-test("Sum(1)(2)", () => {
-  const result = sumMy(1)(2);
-  expect(result).toBe(3);
-});
